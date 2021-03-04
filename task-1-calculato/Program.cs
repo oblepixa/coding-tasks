653edaa0e5958920f97e33d26dc885cd6b2f9f75
pix <oblepixa1999@mail.ru>
2021-03-04 09:58:24 +0300

    Калькулятор через функции

--- task-1-calculato/Program.cs
+++ task-1-calculato/Program.cs
@@ -6,66 +6,89 @@ namespace lab1
     {
         static void Main(string[] args)
         {
-            double a;
-            double b;
-            double total;
-            char oper;
-            Console.WriteLine("Калькулятор");
+
             while (true)
             {
-                Console.WriteLine(' ');
-                Console.WriteLine("");
-                Console.WriteLine("Введите первое число: ");
-                a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите число, операцию и второе число через Enter");
 
-                Console.WriteLine("Введите оператор: ");
-                oper = Convert.ToChar(Console.ReadLine().Substring(0, 1));
-
-                if (oper == '^')
+                try
                 {
-                    total = a * a;
-                    Console.WriteLine("Возведение в квадрат равно " + total + ".");
+                    Console.WriteLine("Ответ: " + Calculate(
+                            Convert.ToDouble(Console.ReadLine()),
+                            Convert.ToChar(Console.ReadLine()),
+                            Convert.ToDouble(Console.ReadLine())
+                        )
+                    );
                 }
-                else
+                catch (DivideByZeroException)
                 {
-                    Console.WriteLine("Введите второе число: ");
-                    b = Convert.ToDouble(Console.ReadLine());
-
-                    if (oper == '+')
-                    {
-                        total = a + b;
-                        Console.WriteLine("Cумма равна " + total + ".");
-                    }
-
-                    else if (oper == '-')
-                    {
-                        total = a - b;
-                        Console.WriteLine("Разность равна " + total + ".");
-                    }
-
-                    else if (oper == '*')
-                    {
-                        total = a * b;
-                        Console.WriteLine("Умножение равно " + total + ".");
-                    }
-
-                    else if (oper == '/')
-                    {
-                        total = a / b;
-                        Console.WriteLine("Деление равно " + total + ".");
-                        if (b == 0)
-                        {
-                            Console.WriteLine("Деление на 0 невозможно.");
-                        }
-                    }
-                    else
-                    {
-                        Console.WriteLine("Неизвестный оператор.");
-                    }
+                    Console.WriteLine("Деление на ноль невозможно");
+                    Console.ReadLine();
+                    continue;
                 }
-                Console.ReadKey();
+                catch (FormatException)
+                {
+                    Console.WriteLine("Вы ввели не число, попробуйте ещё раз!");
+                    Console.ReadLine();
+                    continue;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Вы ввели недопустимую операцию, попробуйте ещё раз!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+            }
+        }
+        static double Calculate(double x, char oper, double y)
+        {
+            switch (oper)
+            {
+                case '+':
+                    return Add(x, y);
+
+                case '-':
+                    return Subtract(x, y);
+
+                case '*':
+                    return Multiply(x, y);
+
+                case '/':
+                    if (y == 0)
+                    {
+                        throw new DivideByZeroException();
+                    }
+                    return Divide(x, y);
+
+                case '^':
+                    return Power(x, y);
+
+                default:
+                    throw new Exception();
             }
 
         }
+        static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
+        static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+        static double Power(double x, double y)
+        {
+            return x * x;
+        }
+
     }
 }
