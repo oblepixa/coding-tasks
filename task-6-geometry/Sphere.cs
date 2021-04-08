47701f56af04210697dfb10ae734abd039eba3cd
Михаил Чистяков <chistyakovmyu.20@edu.ystu.ru>
2021-04-08 09:55:50 +0300

    тплаба7

---
+++ task-6-geometry/Sphere.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_6_circles
+{
+    class Sphere : ISphere
+    {
+        //свойство класса, куда будет сохраняться радиус
+        //его не видно извне, работает только внутри класса
+        private double _radius;
+
+        public void SetRadius(double radius)
+        {
+            _radius = radius;
+        }
+
+        public double Volume()
+        {
+            return (4 *_radius  * _radius * _radius * Math.PI)/3 ;
+        }
+    }
+}
