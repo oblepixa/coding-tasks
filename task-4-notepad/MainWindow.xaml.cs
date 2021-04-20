40d0d0806d8329a1e076a326decb40e2b713002f
pix <oblepixa1999@mail.ru>
2021-04-20 16:33:00 +0300

    cerf

---
+++ task-4-notepad/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.Windows.Controls;
+
+namespace WpfTutorialSamples.Rich_text_controls
+{
+	public partial class RichTextEditorSample : Window
+	{
+		public RichTextEditorSample()
+		{
+			InitializeComponent();
+			cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+			cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+		}
+
+		private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+		{
+			object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty);
+			btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
+			btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+			btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+			cmbFontFamily.SelectedItem = temp;
+			temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty);
+			cmbFontSize.Text = temp.ToString();
+		}
+		private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			if (cmbFontFamily.SelectedItem != null)
+				rtbEditor.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmbFontFamily.SelectedItem);
+		}
+
+		private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontSize.Text);
+		}
+		private void c0_Click(object sender, RoutedEventArgs e)
+		{
+			rtbEditor.Background = Brushes.White;
+			rtbEditor.Foreground = Brushes.Black;
+		}
+		private void c1_Click(object sender, RoutedEventArgs e)
+        {
+			rtbEditor.Background = Brushes.Green;
+			rtbEditor.Foreground = Brushes.Red;
+		}
+		private void c2_Click(object sender, RoutedEventArgs e)
+		{
+			rtbEditor.Background = Brushes.Red;
+			rtbEditor.Foreground = Brushes.SeaGreen;
+		}
+		private void c3_Click(object sender, RoutedEventArgs e)
+		{
+			rtbEditor.Background = Brushes.Yellow;
+			rtbEditor.Foreground = Brushes.Purple;
+		}
+		private void cl_Click(object sender, RoutedEventArgs e)
+		{
+			rtbEditor.Document.Blocks.Clear();
+		}
+
+	}
+
+}
