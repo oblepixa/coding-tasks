69d70739c766152dee857dbb858e4cff24a9dd65
oblepixa <60112915+oblepixa@users.noreply.github.com>
2021-04-21 23:16:17 +0300

    Add files via upload

---
+++ task-9/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFBindingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ObservableCollection<Phone> _phones;
+        // коллекция, которую видно при обновлении
+        public ObservableCollection<Phone> Phones
+        {
+            get
+            {
+                return _phones;
+            }
+            set
+            {
+                _phones = value;
+                NotifyPropertyChanged("Phones");
+            }
+        }
+
+        public MainWindow()
+        {
+            Phones = new ObservableCollection<Phone>();
+            InitializeComponent();
+            // привязывание коллекции как источнику данных для листбокса
+            PhonesListBox.ItemsSource = Phones;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Phones.Add(new Phone("Untitled", 10000, 9644820077));
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PhonesListBox.SelectedItem != null)
+                Phones.Remove(PhonesListBox.SelectedItem as Phone);
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ task-9/Phone.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFBindingExample
+{
+    public class Phone : INotifyPropertyChanged	//важно унаследовать интерфейс для работы с байндингами
+    {
+		//внутренние скрытые поля
+		private string _name;
+		private int _price;
+		private long _number;
+		const int MaxLength = 11;
+
+		//внешнее поле с именем
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				if (_name != value)
+				{
+					_name = value;
+					//вызываем функцию, которая сообщает об обновлении поля Name
+					this.NotifyPropertyChanged("Name");
+				}
+			}
+		}
+
+		//внешнее поле с ценой
+		public int Price
+		{
+			get
+			{
+				return _price;
+			}
+			set
+			{
+				if (_price != value)
+				{
+					bool Changed = int.TryParse(value.ToString(), out _price);
+					//вызываем функцию, которая сообщает об обновлении поля Price
+					if (Changed) this.NotifyPropertyChanged("Price");
+				}
+			}
+		}
+		public long Number
+		{
+			get
+			{
+				return _number;
+			}
+			set
+			{
+				if (_number != value)
+				{ 
+					_number = value;
+					if (_number > 10000000000 )
+                    {
+						_number = 0;
+					}
+					
+				//вызываем функцию, которая сообщает об обновлении поля Name
+				this.NotifyPropertyChanged("Number");
+				}
+			}
+		}
+
+		//пустой конструктор класса
+		public Phone() { }
+
+		//конструктор класса для заполнения при создании объекта
+		public Phone(string Name, int Price, long Number)
+        {
+			this.Name = Name;
+			this.Price = Price;
+			this.Number = Number;
+        }
+
+		//событие, которое срабатывает при обновлении объекта
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		//функция, которую надо запустить, чтобы сообщить об обновлении объекта
+		public void NotifyPropertyChanged(string propName)
+		{
+			if (this.PropertyChanged != null)
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+		}
+	}
+}
