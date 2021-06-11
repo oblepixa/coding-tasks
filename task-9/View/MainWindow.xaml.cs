d4aa651bab1352672fbb32d23d27cf8767154988
oblepixa <60112915+oblepixa@users.noreply.github.com>
2021-06-11 10:53:27 +0300

    Add files via upload

---
+++ task-9/View/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using WPFBindingExample.ViewModels;
+
+namespace WPFBindingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        Class1 handler;
+        
+
+        public MainWindow()
+        {
+            handler = new Class1();
+            InitializeComponent();
+            // привязывание коллекции как источнику данных для листбокса
+            PhonesListBox.ItemsSource = handler.Phones;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                handler.Add(NameTextBox.Text, Convert.ToInt32(PriceTextBox.Text), Convert.ToInt64(NumberTextBox.Text));
+            }
+            catch (FormatException ex)
+            {
+
+            }
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            handler.Remove(PhonesListBox.SelectedItem);
+        }
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            handler.Save();
+        }
+        private void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            handler.Open();
+        }
+
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
---
+++ task-9/ViewModels/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFBindingExample.ViewModels
+{
+    public class Class1
+    {
+        private ObservableCollection<Phone> _phones;
+        // коллекция, которую видно при обновлении
+        public ObservableCollection<Phone> Phones
+        {
+            get
+            {
+                return _phones;
+            }
+            set
+            {
+                _phones = value;
+                NotifyPropertyChanged("Phones");
+            }
+        }
+
+        public Class1()
+        {
+            Phones = new ObservableCollection<Phone>();
+        }
+
+        public void Add(string Name, int Price, long Number)
+        {
+                Phones.Add(new Phone(Name, Price, Number)); 
+        }
+        public void Remove(object SelectedPhone)
+        {
+            if (SelectedPhone != null)
+                Phones.Remove(SelectedPhone as Phone);
+        }
+        public void Save()
+        {
+            using (StreamWriter sw = new StreamWriter("file1.txt"))
+            {
+                foreach (Phone phone in _phones)
+                {
+                    sw.WriteLine(phone.Name + "/" + phone.Number + "/" + phone.Price);
+                }
+
+            }
+        }
+        public void Open()
+
+        {
+            string line = "";
+            using (StreamReader sr = new StreamReader("file1.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                {
+                    try
+                    {
+                        Add(line.Split('/')[0], Convert.ToInt32(line.Split('/')[2]), Convert.ToInt64(line.Split('/')[1]));
+                    }catch (Exception e)
+                    {
+
+                    }
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+
+}
